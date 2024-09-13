b1300d6b6c4a1305575dd772c18aac3208d30e94
JcFiscus <129557433+JcFiscus@users.noreply.github.com>
2024-09-13 16:39:39 -0500

    Update paddle.js

--- BRICK BREAKER/js/paddle.js
+++ BRICK BREAKER/js/paddle.js
@@ -8,8 +8,10 @@ export default class Paddle {
     this.height = 20;
     this.maxSpeed = 7;
     this.speed = 0;
-    this.x = (gameWidth - this.width) / 2;
-    this.y = gameHeight - this.height - 10;
+    this.position = {
+      x: (gameWidth - this.width) / 2,
+      y: gameHeight - this.height - 10
+    };
   }
 
   moveLeft() {
@@ -25,17 +27,17 @@ export default class Paddle {
   }
 
   update(deltaTime) {
-    this.x += this.speed;
+    this.position.x += this.speed;
 
     // Wall collision detection
-    if (this.x < 0) this.x = 0;
-    if (this.x + this.width > this.gameWidth)
-      this.x = this.gameWidth - this.width;
+    if (this.position.x < 0) this.position.x = 0;
+    if (this.position.x + this.width > this.gameWidth)
+      this.position.x = this.gameWidth - this.width;
   }
 
   draw(ctx) {
     ctx.fillStyle = '#0095DD';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
   expand() {
